1c73c1d3fdffce05174393b04102166aee31c117
Dylan <celticdyl@gmail.com>
2022-06-26 15:15:23 -0400

    Roster init, add data, few styles

--- src/components/Roster/Roster.js
+++ src/components/Roster/Roster.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
 import './Roster.css';
+import Papa from 'papaparse';
+import rosterCSV from '../../db/roster_trimmed.csv';
 
 function Roster() {
+
+  const [roster, setRoster] = useState([]);
+
+  Papa.parse(rosterCSV, {
+    download: true,
+    complete: function (input) {
+      const records = input.data;
+      setRoster(records);
+      console.log(roster)
+    }
+  });
+
   return (
     <div className='mask'>
-        <div className='container bg-primary'>
-           Hi this is Roster
-        </div>
+      <div className='roster-table'>
+        <table className='table table-striped'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Position</th>
+            </tr>
+          </thead>
+          <tbody>
+            {roster.map((player, index) => {
+              return (
+                <tr key={index}>
+                  <td>{player[0]}</td>
+                  <td>{player[1]}</td>
+                </tr>
+              )
+            }
+            )}
+          </tbody>
+        </table>
+
+      </div>
     </div>
   );
 }
