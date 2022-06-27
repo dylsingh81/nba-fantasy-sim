2d17b1408ba9fc91f530c7f511e4515b1aa0bc6d
Dylan <celticdyl@gmail.com>
2022-06-27 19:20:24 -0400

    Player pics

--- src/components/Roster/Roster.js
+++ src/components/Roster/Roster.js
@@ -4,24 +4,30 @@ import Papa from 'papaparse';
 import rosterCSV from '../../db/roster_trimmed.csv';
 import RosterTable from "./RosterTable/RosterTable";
 
+var dataLoaded = false;
+
 function Roster() {
 
   const [roster, setRoster] = useState([]);
-
-  Papa.parse(rosterCSV, {
-    download: true,
-    complete: function (input) {
-      const records = input.data;
-      setRoster(records);
-      console.log(roster)
-    }
-  });
+  if(!dataLoaded){
+    Papa.parse(rosterCSV, {
+      download: true,
+      header: true,
+      complete: function (input) {
+        const records = input.data;
+        setRoster(records);
+        console.log("Data Loaded")
+        dataLoaded = true
+      }
+    });
+  }
+  
 
   return (
     <div className='roster-mask'>
       <div className='roster-table'>
         
-        <RosterTable></RosterTable>
+        <RosterTable roster={roster}></RosterTable>
 
       </div>
     </div>
--- src/components/Roster/RosterTable/RosterTable.js
+++ src/components/Roster/RosterTable/RosterTable.js
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import './RosterTable.css';
 import {
   useTable
-  ,useSortBy
-  ,usePagination
-  ,useFilters
-  
-  
+  , useSortBy
+  , usePagination
+  , useFilters
+
+
 } from "react-table";
+
+// Import multiple images from a folder
+
+
+
+
 
 // From :
 // https://akashmittal.com/gui-utility-to-generate-react-table-code/
@@ -63,112 +69,12 @@ function SelectColumnFilter({
     </select>
   );
 }
-// Select filter function end
-
-
-
-// Function for slider range filter
-function SliderColumnFilter({
-  column: { filterValue, setFilter, preFilteredRows, id }
-}) {
-  // Calculate the min and max
-  // using the preFilteredRows
-
-  const [min, max] = React.useMemo(() => {
-    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0;
-    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0;
-    preFilteredRows.forEach((row) => {
-      min = Math.min(row.values[id], min);
-      max = Math.max(row.values[id], max);
-    });
-    return [min, max];
-  }, [id, preFilteredRows]);
-
-  return (
-    <>
-      <input
-        type="range"
-        min={min}
-        max={max}
-        value={filterValue || min}
-        onChange={(e) => {
-          setFilter(parseInt(e.target.value, 10));
-        }}
-      />
-      <span>{filterValue || min}</span>{" "}
-      <button onClick={() => setFilter(undefined)}>Off</button>
-    </>
-  );
-}
-
-
-
-// Slider filter function end
-
-// Function for Min-Max range filter
-function NumberRangeColumnFilter({
-  column: { filterValue = [], preFilteredRows, setFilter, id }
-}) {
-  const [min, max] = React.useMemo(() => {
-    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0;
-    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0;
-    preFilteredRows.forEach((row) => {
-      min = Math.min(row.values[id], min);
-      max = Math.max(row.values[id], max);
-    });
-    return [min, max];
-  }, [id, preFilteredRows]);
-
-  return (
-    <div
-      style={{
-        display: "flex"
-      }}
-    >
-      <input
-        value={filterValue[0] || ""}
-        type="number"
-        onChange={(e) => {
-          const val = e.target.value;
-          setFilter((old = []) => [
-            val ? parseInt(val, 10) : undefined,
-            old[1]
-          ]);
-        }}
-        placeholder={`Min (${min})`}
-        style={{
-          width: "70px",
-          marginRight: "0.5rem"
-        }}
-      />
-      to
-      <input
-        value={filterValue[1] || ""}
-        type="number"
-        onChange={(e) => {
-          const val = e.target.value;
-          setFilter((old = []) => [
-            old[0],
-            val ? parseInt(val, 10) : undefined
-          ]);
-        }}
-        placeholder={`Max (${max})`}
-        style={{
-          width: "70px",
-          marginLeft: "0.5rem"
-        }}
-      />
-    </div>
-  );
-}
-// Min-Max range filter function end
-
 
 
 // Table function. It creates UI.
 
-function Table({columns, data}) {
-  
+function Table({ columns, data }) {
+
   const filterTypes = React.useMemo(
     () => ({
       text: (rows, id, filterValue) => {
@@ -176,8 +82,8 @@ function Table({columns, data}) {
           const rowValue = row.values[id];
           return rowValue !== undefined
             ? String(rowValue)
-                .toLowerCase()
-                .startsWith(String(filterValue).toLowerCase())
+              .toLowerCase()
+              .startsWith(String(filterValue).toLowerCase())
             : true;
         });
       }
@@ -192,16 +98,16 @@ function Table({columns, data}) {
     }),
     []
   );
-  
-  
+
+
 
   // Use the state and functions returned from useTable to build your UI
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    footerGroups,
-    rows,
+    //footerGroups,
+    //rows,
     prepareRow,
     page,
     canPreviousPage,
@@ -213,7 +119,7 @@ function Table({columns, data}) {
     previousPage,
     setPageSize,
     state: { pageIndex, pageSize, },
-    
+
   } = useTable(
     {
       columns,
@@ -222,7 +128,7 @@ function Table({columns, data}) {
       filterTypes
     },
     useFilters,
-    
+
     useSortBy,
     usePagination,
   );
@@ -230,7 +136,7 @@ function Table({columns, data}) {
   // Render the UI for your table
   return (
     <>
-      
+
       <table
         {...getTableProps()}
         border={1}
@@ -239,38 +145,39 @@ function Table({columns, data}) {
           width: "100%",
           margin: "auto"
         }}
+        className = "table"
       >
-        
+
         <thead>
           {headerGroups.map((group) => (
             <tr {...group.getHeaderGroupProps()}>
               {group.headers.map((column) => (
                 <th {...column.getHeaderProps()}>
-                  
-                    <div {...column.getSortByToggleProps()}>
-                      {column.render("Header")}
-                      <span>
-                        {column.isSorted
-                          ? column.isSortedDesc
-                            ? " 🔽"
-                            : " 🔼"
-                          : column.canSort
+
+                  <div {...column.getSortByToggleProps()}>
+                    {column.render("Header")}
+                    <span>
+                      {column.isSorted
+                        ? column.isSortedDesc
+                          ? " 🔽"
+                          : " 🔼"
+                        : column.canSort
                           ? "⏺"
                           : ""}
-                      </span>
-                    </div>
-                  
-                  
-                    <div>
-                      {column.canFilter ? column.render("Filter") : null}
-                    </div>
-                  
+                    </span>
+                  </div>
+
+
+                  <div>
+                    {column.canFilter ? column.render("Filter") : null}
+                  </div>
+
                 </th>
               ))}
             </tr>
           ))}
         </thead>
-        
+
 
         <tbody {...getTableBodyProps()}>
           {page.map((row, i) => {
@@ -278,17 +185,47 @@ function Table({columns, data}) {
             return (
               <tr {...row.getRowProps()}>
                 {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  );
+                  //if first column
+                  if (cell.column.id === 'columnId_00.27830976550007236') {
+                    if (row.original["Image Available"] === "Yes") {
+                      //console.log("Image Available");
+                      //get player name
+                      var playerName = row.original.name;
+                      //get player image; 
+                      //put dash between spaces in player name
+                      var playerNameDash = playerName.replace(/\s/g, "-");
+                      //remove any special characters
+                      var playerNameDashNoSpecial = playerNameDash.replace(/[^a-zA-Z0-9-]/g, "");
+                      // add path to player image
+                      var playerImagePath = require("../../player_images/" + playerNameDashNoSpecial + ".png");
+                      
+
+                      return (
+                        <td key="key"> 
+                          <img src={playerImagePath} className = "player-icon" alt="Logo" />
+                        </td>
+                      )
+                    }
+                    else {
+                      //console.log("No Image")
+                      return (
+                        <td key=""> No </td>
+                      );
+                    }
+                  }
+                  else {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  }
                 })}
               </tr>
             );
           })}
         </tbody>
-        
+
       </table>
-      
+
       <div className="pagination">
         <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
           {"<<"}
@@ -336,13 +273,15 @@ function Table({columns, data}) {
           ))}
         </select>
       </div>
-      
+
     </>
   );
 }
 
 // App component start
-function RosterTable() {
+
+//RosterTable with roster prop
+function RosterTable({ roster }) {
 
   // Columns array. This array contains your table headings and accessors which maps keys from data array
   const columns = React.useMemo(() => (
@@ -359,14 +298,15 @@ function RosterTable() {
         "id": "columnId_00.8366168306389434",
         "Header": "Name",
         "Footer": "",
-        "accessor": "Key1"
+        "accessor": "name"
       },
       {
         "id": "columnId_00.9080443947570558",
         "Header": "Team",
         "Footer": "",
         "accessor": "Key2",
-        "filter": "includes"
+        Filter: SelectColumnFilter,
+
       },
       {
         "id": "columnId_00.40874175307236293",
@@ -391,415 +331,9 @@ function RosterTable() {
     ]
   ), []);
 
-  // Data array. Replace it with your actual data.
-  const data = React.useMemo(() => ([
-    {
-      "Key0": 1,
-      "Key1": "Julian",
-      "Key2": "Ms",
-      "Key3": "Female",
-      "Key4": 1997,
-      "Key5": 1
-    },
-    {
-      "Key0": 2,
-      "Key1": "Ketti",
-      "Key2": "Dr",
-      "Key3": "Female",
-      "Key4": 1990,
-      "Key5": 4
-    },
-    {
-      "Key0": 3,
-      "Key1": "Fallon",
-      "Key2": "Rev",
-      "Key3": "Female",
-      "Key4": 1993,
-      "Key5": 2
-    },
-    {
-      "Key0": 4,
-      "Key1": "Leela",
-      "Key2": "Dr",
-      "Key3": "Female",
-      "Key4": 1991,
-      "Key5": 5
-    },
-    {
-      "Key0": 5,
-      "Key1": "Vivienne",
-      "Key2": "Rev",
-      "Key3": "Female",
-      "Key4": 2004,
-      "Key5": 2
-    },
-    {
-      "Key0": 6,
-      "Key1": "Margaux",
-      "Key2": "Ms",
-      "Key3": "Male",
-      "Key4": 2002,
-      "Key5": 3
-    },
-    {
-      "Key0": 7,
-      "Key1": "Mannie",
-      "Key2": "Rev",
-      "Key3": "Male",
-      "Key4": 2011,
-      "Key5": 3
-    },
-    {
-      "Key0": 8,
-      "Key1": "Vanya",
-      "Key2": "Mrs",
-      "Key3": "Female",
-      "Key4": 2012,
-      "Key5": 2
-    },
-    {
-      "Key0": 9,
-      "Key1": "Barbabra",
-      "Key2": "Rev",
-      "Key3": "Female",
-      "Key4": 2006,
-      "Key5": 5
-    },
-    {
-      "Key0": 10,
-      "Key1": "Fiann",
-      "Key2": "Mr",
-      "Key3": "Female",
-      "Key4": 2006,
-      "Key5": 5
-    },
-    {
-      "Key0": 11,
-      "Key1": "Sargent",
-      "Key2": "Ms",
-      "Key3": "Female",
-      "Key4": 1994,
-      "Key5": 1
-    },
-    {
-      "Key0": 12,
-      "Key1": "Josy",
-      "Key2": "Rev",
-      "Key3": "Female",
-      "Key4": 2011,
-      "Key5": 4
-    },
-    {
-      "Key0": 13,
-      "Key1": "Hulda",
-      "Key2": "Honorable",
-      "Key3": "Female",
-      "Key4": 2010,
-      "Key5": 2
-    },
-    {
-      "Key0": 14,
-      "Key1": "Giavani",
-      "Key2": "Ms",
-      "Key3": "Male",
-      "Key4": 1993,
-      "Key5": 2
-    },
-    {
-      "Key0": 15,
-      "Key1": "Berkley",
-      "Key2": "Ms",
-      "Key3": "Male",
-      "Key4": 1999,
-      "Key5": 2
-    },
-    {
-      "Key0": 16,
-      "Key1": "Nick",
-      "Key2": "Honorable",
-      "Key3": "Male",
-      "Key4": 2005,
-      "Key5": 5
-    },
-    {
-      "Key0": 17,
-      "Key1": "Angelina",
-      "Key2": "Rev",
-      "Key3": "Female",
-      "Key4": 1994,
-      "Key5": 4
-    },
-    {
-      "Key0": 18,
-      "Key1": "Guglielmo",
-      "Key2": "Honorable",
-      "Key3": "Female",
-      "Key4": 1986,
-      "Key5": 3
-    },
-    {
-      "Key0": 19,
-      "Key1": "Myrtie",
-      "Key2": "Mr",
-      "Key3": "Female",
-      "Key4": 2012,
-      "Key5": 3
-    },
-    {
-      "Key0": 20,
-      "Key1": "Oates",
-      "Key2": "Honorable",
-      "Key3": "Male",
-      "Key4": 1966,
-      "Key5": 3
-    },
-    {
-      "Key0": 21,
-      "Key1": "Griswold",
-      "Key2": "Rev",
-      "Key3": "Male",
-      "Key4": 1995,
-      "Key5": 3
-    },
-    {
-      "Key0": 22,
-      "Key1": "Paxton",
-      "Key2": "Rev",
-      "Key3": "Male",
-      "Key4": 1992,
-      "Key5": 5
-    },
-    {
-      "Key0": 23,
-      "Key1": "Inesita",
-      "Key2": "Mr",
-      "Key3": "Male",
-      "Key4": 1992,
-      "Key5": 1
-    },
-    {
-      "Key0": 24,
-      "Key1": "Charleen",
-      "Key2": "Honorable",
-      "Key3": "Male",
-      "Key4": 2003,
-      "Key5": 4
-    },
-    {
-      "Key0": 25,
-      "Key1": "Rowland",
-      "Key2": "Rev",
-      "Key3": "Male",
-      "Key4": 2010,
-      "Key5": 4
-    },
-    {
-      "Key0": 26,
-      "Key1": "Trueman",
-      "Key2": "Rev",
-      "Key3": "Male",
-      "Key4": 1994,
-      "Key5": 3
-    },
-    {
-      "Key0": 27,
-      "Key1": "Mathilda",
-      "Key2": "Rev",
-      "Key3": "Male",
-      "Key4": 2011,
-      "Key5": 1
-    },
-    {
-      "Key0": 28,
-      "Key1": "Melloney",
-      "Key2": "Honorable",
-      "Key3": "Female",
-      "Key4": 1995,
-      "Key5": 2
-    },
-    {
-      "Key0": 29,
-      "Key1": "Inglis",
-      "Key2": "Rev",
-      "Key3": "Female",
-      "Key4": 1993,
-      "Key5": 5
-    },
-    {
-      "Key0": 30,
-      "Key1": "Giustina",
-      "Key2": "Mr",
-      "Key3": "Male",
-      "Key4": 1992,
-      "Key5": 2
-    },
-    {
-      "Key0": 31,
-      "Key1": "Conrad",
-      "Key2": "Mrs",
-      "Key3": "Male",
-      "Key4": 1989,
-      "Key5": 5
-    },
-    {
-      "Key0": 32,
-      "Key1": "Emmalynne",
-      "Key2": "Dr",
-      "Key3": "Male",
-      "Key4": 2004,
-      "Key5": 4
-    },
-    {
-      "Key0": 33,
-      "Key1": "Jude",
-      "Key2": "Rev",
-      "Key3": "Female",
-      "Key4": 1992,
-      "Key5": 3
-    },
-    {
-      "Key0": 34,
-      "Key1": "Farlee",
-      "Key2": "Honorable",
-      "Key3": "Male",
-      "Key4": 2005,
-      "Key5": 2
-    },
-    {
-      "Key0": 35,
-      "Key1": "Mendel",
-      "Key2": "Honorable",
-      "Key3": "Female",
-      "Key4": 2010,
-      "Key5": 5
-    },
-    {
-      "Key0": 36,
-      "Key1": "Fae",
-      "Key2": "Dr",
-      "Key3": "Female",
-      "Key4": 1994,
-      "Key5": 2
-    },
-    {
-      "Key0": 37,
-      "Key1": "Janella",
-      "Key2": "Rev",
-      "Key3": "Female",
-      "Key4": 1992,
-      "Key5": 3
-    },
-    {
-      "Key0": 38,
-      "Key1": "Esmeralda",
-      "Key2": "Ms",
-      "Key3": "Male",
-      "Key4": 1987,
-      "Key5": 4
-    },
-    {
-      "Key0": 39,
-      "Key1": "Quintilla",
-      "Key2": "Ms",
-      "Key3": "Male",
-      "Key4": 2008,
-      "Key5": 3
-    },
-    {
-      "Key0": 40,
-      "Key1": "Devland",
-      "Key2": "Rev",
-      "Key3": "Female",
-      "Key4": 2010,
-      "Key5": 1
-    },
-    {
-      "Key0": 41,
-      "Key1": "Preston",
-      "Key2": "Mrs",
-      "Key3": "Male",
-      "Key4": 2008,
-      "Key5": 2
-    },
-    {
-      "Key0": 42,
-      "Key1": "Quintilla",
-      "Key2": "Dr",
-      "Key3": "Male",
-      "Key4": 1993,
-      "Key5": 1
-    },
-    {
-      "Key0": 43,
-      "Key1": "Minor",
-      "Key2": "Mrs",
-      "Key3": "Male",
-      "Key4": 2010,
-      "Key5": 1
-    },
-    {
-      "Key0": 44,
-      "Key1": "Jody",
-      "Key2": "Mrs",
-      "Key3": "Male",
-      "Key4": 1985,
-      "Key5": 5
-    },
-    {
-      "Key0": 45,
-      "Key1": "Shanda",
-      "Key2": "Mr",
-      "Key3": "Female",
-      "Key4": 2004,
-      "Key5": 2
-    },
-    {
-      "Key0": 46,
-      "Key1": "Sheelagh",
-      "Key2": "Mrs",
-      "Key3": "Male",
-      "Key4": 2004,
-      "Key5": 2
-    },
-    {
-      "Key0": 47,
-      "Key1": "Ester",
-      "Key2": "Ms",
-      "Key3": "Female",
-      "Key4": 2004,
-      "Key5": 1
-    },
-    {
-      "Key0": 48,
-      "Key1": "Anita",
-      "Key2": "Rev",
-      "Key3": "Male",
-      "Key4": 2008,
-      "Key5": 5
-    },
-    {
-      "Key0": 49,
-      "Key1": "Aurelea",
-      "Key2": "Mr",
-      "Key3": "Male",
-      "Key4": 2003,
-      "Key5": 3
-    },
-    {
-      "Key0": 50,
-      "Key1": "Aldis",
-      "Key2": "Ms",
-      "Key3": "Male",
-      "Key4": 1996,
-      "Key5": 3
-    }
-  ]), []);
-
-
   return (
-    <Table columns={columns} data={data} />
+    <Table columns={columns} data={roster} />
   );
 }
-// App component end
 
 export default RosterTable;
