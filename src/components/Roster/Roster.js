0e48e876efaed878fea6be18b7934319a71575ba
Dylan <celticdyl@gmail.com>
2022-06-28 21:07:52 -0400

    todo

--- src/components/Roster/Roster.js
+++ src/components/Roster/Roster.js
@@ -26,6 +26,7 @@ function Roster() {
 
   return (
     <div className='roster-mask'>
+      <div>Historic Current All</div>
       <div className='roster-table'>
         <RosterTable roster={roster}></RosterTable>
       </div>
