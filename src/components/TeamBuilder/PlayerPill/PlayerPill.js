ff28133df0f19f96162d8b4e731f8ed4a8427c48
Dylan <celticdyl@gmail.com>
2022-07-06 21:30:05 -0400

    updates to team builder

--- src/components/TeamBuilder/PlayerPill/PlayerPill.js
+++ src/components/TeamBuilder/PlayerPill/PlayerPill.js
@@ -2,17 +2,28 @@ import React from 'react';
 import './PlayerPill.css';
 
 function PlayerPill(props) {
+    console.log(props.player)
+    var playerImagePath;
+    
 
-    var playerImagePath1 = require("../../../db/player_images/mystery.png");
+    if(props.player.playerid < 0) {
+        playerImagePath = require("../../../db/player_images/mystery.png");
 
-    if (props.type === "starter") {
+    }
+    else{
+        // Replace space in name with dash
+        var playerName = props.player.name.replace(/\s/g, "-");
+        playerImagePath = require("../../../db/player_images/" + playerName + ".png");
+    }
+
+    if (props.player.type === "starter") {
         return (
-            <div className="player-starter-pill rounded-pill row align-items-center">
+            <div className="player-starter-pill rounded-pill row align-items-center" onClick={props.handleShow}>
                 <div className='col-md-1'>
                     <h3>{props.position}</h3>
                 </div>
                 <div className='col-md-3 line-sep'>
-                    <img src={playerImagePath1} className="player-image" alt="NBA PLAYER" />
+                    <img src={playerImagePath} className="player-image" alt="NBA PLAYER" />
                 </div>
                 <div className='col-md-4'>
                     <h2>~Player Name~</h2>
@@ -22,11 +33,11 @@ function PlayerPill(props) {
     }
 
     //if reserve
-    if (props.type === "reserve"){
+    if (props.player.type === "reserve"){
         return (
-            <div className="player-reserve-pill rounded-pill row align-items-center">
+            <div className="player-reserve-pill rounded-pill row align-items-center" onClick={props.handleShow}>
                 <div className='col-md-4'>
-                    <img src={playerImagePath1} className="player-image" alt="NBA PLAYER" />
+                    <img src={playerImagePath} className="player-image" alt="NBA PLAYER" />
                 </div>
                 <div className='col-md-8'>
                     <h3>~Player Name~</h3>
--- src/components/TeamBuilder/TeamBuilder.js
+++ src/components/TeamBuilder/TeamBuilder.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import PlayerPill from './PlayerPill/PlayerPill';
 import './TeamBuilder.css';
+//Import use state
+import { useState } from "react";
+import { Offcanvas } from 'react-bootstrap';
 
 function TeamBuilder() {
+
+  
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  // Set players to an array of 8 empty objects
+  
+  const [playersArray, setplayersArray] = useState([]);
+  //Set players array to 8 dicts with player id equal to -1, position equal to "", and type equal to "starter"
+  for (let i = 0; i < 8; i++) {
+    playersArray.push({
+      playerid: -1,
+      position: "",
+      type: "starter",
+      name: ""
+    });
+  }
+
+  // Set final 3 of players array to type
+  for (let i = playersArray.length-1; i > playersArray.length-4; i--) {
+    playersArray[i].type = "reserve";
+  }
+
+  // Set first player in players array to name Jayson Tatum
+  playersArray[0].name = "Jayson Tatum";
+  playersArray[0].playerid = 0;
 
   return (
     <div className='team-mask'>
 
-      <span id="team-intro"> 
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+
+      <span id="team-intro">
         <h3>Welcome to TeamBuilder!</h3>
         Click on the buttons to select players and Save/Download your team when done!
       </span>
@@ -16,7 +56,7 @@ function TeamBuilder() {
         <div className='col-md-4'>
           <span>Budget Total:</span>
         </div>
-        
+
         <div className='col-md-4'>
           <span>Budget Spent:</span>
         </div>
@@ -30,64 +70,64 @@ function TeamBuilder() {
       <div className=" team-container">
         <div className='row header'>
           <div className='col-md-8 line-right'>
-              <h3>Starters</h3>
+            <h3>Starters</h3>
           </div>
           <div className='col-md-4'>
-              <h3>Reserves</h3>
+            <h3>Reserves</h3>
           </div>
         </div>
 
         <div className='row'>
           <div className='col-md-8 line-right'>
-            <PlayerPill position="PG" type="starter"> </PlayerPill>
+            <PlayerPill position="PG" handleShow={handleShow} player={playersArray[0]}> </PlayerPill>
           </div>
         </div>
 
         <div className='row'>
           <div className='col-md-8 line-right'>
-            <PlayerPill position="SG" type="starter"/>
+            <PlayerPill position="SG" player={playersArray[1]}/>
           </div>
-          <div className='col-md-4' id="first-reserve">
-            <PlayerPill type="reserve"/>
+          <div className='col-md-4' id="first-reserve" >
+            <PlayerPill player={playersArray[5]} />
           </div>
         </div>
 
         <div className='row'>
           <div className='col-md-8 line-right'>
-            <PlayerPill position="SF" type="starter"/>
+            <PlayerPill position="SF" player={playersArray[2]}/>
           </div>
           <div className='col-md-4'>
-            <PlayerPill type="reserve"/>
+            <PlayerPill type="reserve" player={playersArray[6]}/>
           </div>
         </div>
 
         <div className='row'>
           <div className='col-md-8 line-right'>
-            <PlayerPill position="PF" type="starter"/>
+            <PlayerPill position="PF" player={playersArray[3]} />
           </div>
           <div className='col-md-4' id="last-reserve">
-            <PlayerPill type="reserve"/>
+            <PlayerPill type="reserve" player={playersArray[7]}/>
           </div>
         </div>
 
         <div className='row'>
           <div className='col-md-8 line-right'>
-            <PlayerPill position="C" type="starter"/>
+            <PlayerPill position="C" player={playersArray[4]}/>
           </div>
-         
+
         </div>
       </div>
-      
 
-        <div className='row py-4 save-footer'>
-          <div className='col-md-6'>
-            <button className="btn btn-primary">Download Team</button>
-          </div>
-          <div className='col-md-6  '>
+
+      <div className='row py-4 save-footer'>
+        <div className='col-md-6'>
+          <button className="btn btn-primary">Download Team</button>
+        </div>
+        <div className='col-md-6  '>
           <button className="btn btn-success">Save Team</button>
-          </div>
+        </div>
       </div>
-      
+
     </div>
   );
 }
